fd48e48ac9890378c57e96e4b116c5b04d44990d
Caleb Simpson <caleb.simpson@jadedpixel.com>
2013-03-13 09:39:25 -0400

    Add option to specify coverting code -> name.

--- country-converter.rb
+++ country-converter.rb
@@ -8,10 +8,14 @@ class CountryConverter < TerminalRunner
   param "input", "A file containing the list of countries to convert."
 
   option "--help", 0, "", "Shows this help file."
+  option "--name", 0, "", "Output the country name instead of code."
+
   option_alias "--help", "-h"
+  option_alias "--name", "-n"
 
   help <<-EOS
-    There should be one country name per line in the file.
+    There should be one country name per line in the input file.
+    By default the converter goes from name >> code, this can be changed by including the --name flag.
   EOS
 
   def self.run
@@ -27,19 +31,27 @@ class CountryConverter < TerminalRunner
 
     @@countries = YAML::load_file("countries.yml")
 
+    if @@options.include? "--name"
+      input = "code"
+      output = "name"
+    else
+      input = "name"
+      output = "code"
+    end
+
     codes = []
     File.open(@@params["input"], "r") do |infile|
       while (line = infile.gets)
         line = line[0..-2]
-        codes << self.find_country(line)
+        codes << self.find_country(line, input, output)
       end
     end
     puts codes.join(", ")
   end
 
-  def self.find_country(name)
+  def self.find_country(name, input, output)
     @@countries.each do |country|
-      return country["code"] if (country["name"] == name)
+      return country[output] if (country[input] == name)
     end
     "[#{name} - NOT FOUND]"
   end
